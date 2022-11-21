38d56d65e3b00a97e271dc1df44b7f86d1a5486c
jeffxie888 <jeffreyxie888@gmail.com>
2022-11-21 11:05:23 -0800

    replace tr/td with div

--- src/components/NewsRow.tsx
+++ src/components/NewsRow.tsx
@@ -17,8 +17,8 @@ const NewsRow = ({ title, url, time, publisher, imageUrl, subjects }: NewsRowPro
   return (
     <div className="border-b border-y-2 sm:w-full md:w-9/12 py-3 flex flex-col gap-3">
       <a href={url} target="_blank">
-        <tr className="w-full flex flex-row items-center hover:bg-slate-100 hover:cursor-pointer">
-          <td className="w-8/12 mr-3">
+        <div className="w-full flex flex-row items-center hover:bg-slate-100 hover:cursor-pointer">
+          <div className="w-8/12 mr-3">
             <div className="p2 flex flex-col gap-3">
               <div className="text-sm text-gray-500 pb-1">
                 <span className="font-semibold ">{publisher}</span> {"\u2022"}{" "}
@@ -26,15 +26,15 @@ const NewsRow = ({ title, url, time, publisher, imageUrl, subjects }: NewsRowPro
               </div>
               <span className="text-sm">{title}</span>
             </div>
-          </td>
+          </div>
           {imageUrl && (
-            <td className="w-4/12 pl-2">
+            <div className="w-4/12 pl-2">
               <img className="h-auto w-auto rounded" src={imageUrl} />
-            </td>
+            </div>
           )}
-        </tr>
+        </div>
       </a>
-      <tr>
+      <div>
         <div className="flex flex-row gap-2">
           {subjects.map((subject) => {
             const symbol = subject.symbol;
@@ -50,7 +50,7 @@ const NewsRow = ({ title, url, time, publisher, imageUrl, subjects }: NewsRowPro
             );
           })}
         </div>
-      </tr>
+      </div>
     </div>
   );
 };
